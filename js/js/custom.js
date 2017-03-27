17b44d64e1776a509b8484ab67ec40e98d5643e6
Nazih14 <nazihbopas@gmail.com>
2017-03-27 19:08:26 +0700

    update
    update

---
+++ js/js/custom.js
@@ -0,0 +1,73 @@
+
+new WOW().init();
+
+wow = new WOW(
+{
+                      boxClass:     'wow',      // default
+                      animateClass: 'animated', // default
+                      offset:       0,          // default
+                      mobile:       true,       // default
+                      live:         true        // default
+                    }
+                    )
+wow.init();
+
+$(document).ready(function () {
+
+	$('.star').on('click', function () {
+      $(this).toggleClass('star-checked');
+    });
+
+    $('.ckbox label').on('click', function () {
+      $(this).parents('tr').toggleClass('selected');
+    });
+
+    $('.btn-filter').on('click', function () {
+      var $target = $(this).data('target');
+      if ($target != 'all') {
+        $('.table tr').css('display', 'none');
+        $('.table tr[data-status="' + $target + '"]').fadeIn('slow');
+      } else {
+        $('.table tr').css('display', 'none').fadeIn('slow');
+      }
+    });
+
+ });
+
+
+
+
+/* jQuery toggle layout */
+$('#btnToggle').click(function(){
+  if ($(this).hasClass('on')) {
+    $('#main .col-md-6').addClass('col-md-4').removeClass('col-md-6');
+    $(this).removeClass('on');
+  }
+  else {
+    $('#main .col-md-4').addClass('col-md-6').removeClass('col-md-4');
+    $(this).addClass('on');
+  }
+});
+
+$('ul.nav li.dropdown').hover(function() {
+  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
+}, function() {
+  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
+});
+
+
+
+
+
+    $(document).ready(function(){
+      $("#circle").hide();
+      $("#box").animate({height:"280px"},"slow");
+      $("#box").animate({width:"450px"},"slow");
+      $("#circle").fadeIn(1000);
+
+    });
+    function blinker(){
+      $('#blinking').fadeOut("slow");
+      $('#blinking').fadeIn("slow");
+    }
+    setInterval(blinker, 1000);
